9bdfd5fd123392c028db2c4ff468615fd40ae677
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-03-11 21:30:24 -0500

    database , orm , modules

--- src/modules/restaurants/restaurants.resolver.ts
+++ src/modules/restaurants/restaurants.resolver.ts
@@ -1,5 +1,23 @@
-import { Resolver } from "@nestjs/graphql";
+import { Resolver , Query, Args, Mutation } from "@nestjs/graphql";
+import { createRestaurantDto } from "./dtos/create-restaurant.dto";
+import { Restaurant } from "./entities/restaurant.entity";
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
+
+   //  Querys
+   @Query( returns => [Restaurant])
+   restaurants(@Args('vegan')veganOnly:boolean) : Restaurant[] {
+      return [];
+   };
+
+   // Mutations
+   @Mutation (returns => Boolean)
+   createRestaurant(
+      @Args() createRestaurantDto:createRestaurantDto,
+   ):boolean {
+      console.log(createRestaurantDto);
+      return true;
+   }
+
 }
