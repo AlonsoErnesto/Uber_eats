47a75edfc138a928c31a967a3c11b21173d2f725
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-03-14 21:39:44 -0500

    query exmaple service-restaurant

--- src/modules/restaurants/restaurants.resolver.ts
+++ src/modules/restaurants/restaurants.resolver.ts
@@ -1,14 +1,17 @@
 import { Resolver , Query, Args, Mutation } from "@nestjs/graphql";
 import { createRestaurantDto } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
+import { RestaurantService } from "./restaurants.service";
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
 
+   constructor(private readonly restaurantService : RestaurantService){}
+
    //  Querys
    @Query( returns => [Restaurant])
-   restaurants(@Args('vegan')veganOnly:boolean) : Restaurant[] {
-      return [];
+   restaurants(): Promise<Restaurant[]> {
+      return this.restaurantService.getAll();
    };
 
    // Mutations
