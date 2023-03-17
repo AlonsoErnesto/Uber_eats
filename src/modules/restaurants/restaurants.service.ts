c9c43f34c1c0e1f1654c27af9c6e44835388068f
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-03-16 21:57:16 -0500

    using entity for DTO of restaurants

--- src/modules/restaurants/restaurants.service.ts
+++ src/modules/restaurants/restaurants.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+
+
 @Injectable()
 export class RestaurantService {
 
@@ -9,5 +12,10 @@ export class RestaurantService {
 
    getAll():Promise<Restaurant[]>{
       return this.restaurants.find();
+   };
+
+   createRestaurant (createRestaurantDto : createRestaurantDto) : Promise<Restaurant>{
+      const newRestaurant = this.restaurants.create(createRestaurantDto)
+      return this.restaurants.save(newRestaurant);
    }
 }
