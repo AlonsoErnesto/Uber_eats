f4a4ac83d076ef749a257712a829395795e4de7f
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-03-23 21:13:56 -0500

    password update

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/modules/common/entities/core.entity";
-import { BeforeInsert, Column, Entity } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity } from "typeorm";
 import * as bcrypt from 'bcrypt';
 import { InternalServerErrorException } from "@nestjs/common";
 import { IsEmail, IsEnum, IsString } from "class-validator";
@@ -33,7 +33,9 @@ export class User extends CoreEntity{
    @IsEnum(UserRole)
    role:UserRole;
 
+
    @BeforeInsert()
+   @BeforeUpdate()
    async hashPassword():Promise<void> {
       try {
          this.password = await bcrypt.hash(this.password,10);
